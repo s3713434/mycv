635050c1a231b83ee099c9e74a33de92467778a8
Shiyu Chen <42055092+s3713434@users.noreply.github.com>
2024-11-08 18:32:43 +1100

    Working on switchToHttp().getRequest()

--- src/decorators/current-user.decorator.ts
+++ src/decorators/current-user.decorator.ts
@@ -2,6 +2,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
   (data: any, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    console.log(request.session.userId);
     return 'hi there';
   },
 );
